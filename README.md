9eb5c88c2c0b158025d36bbe613c2024e1e77aef
Matias Piano <matias@globalfishingwatch.org>
2019-06-03 23:27:44 -0300

    Fixes handling airflow dag regarding review

--- README.md
+++ README.md
@@ -7,10 +7,19 @@ Chile and Panama will be sending us NAF messages, we need a DAG pipeline that gi
 Need to configure the Airflow Variable configurations like this
 
 ```json
-configurations:[
-{name:"panama", gcs_source:"gs://vms-gfw/panama/real-time-naf",gcs_csv_output:"gs://vms-gfw/panama/naf-to-csv",bq_output:"VMS_Panama.raw_naf_messages"},
-{name:"chile", gcs_source:"gs://vms-gfw/chile/real-time-naf",gcs_csv_output:"gs://vms-gfw/chile/naf-to-csv",bq_output:"VMS_Chile.raw_naf_messages"}
-]
+{
+  "configurations": [{
+    "name": "panama",
+    "gcs_source": "gs://vms-gfw/panama/real-time-naf",
+    "gcs_csv_output": "gs://vms-gfw/panama/naf-to-csv",
+    "bq_output": "VMS_Panama.raw_naf_messages"
+  }, {
+    "name": "chile",
+    "gcs_source": "gs://vms-gfw/chile/real-time-naf",
+    "gcs_csv_output": "gs://vms-gfw/chile/naf-to-csv",
+    "bq_output": "VMS_Chile.raw_naf_messages"
+  }]
+}
 ```
 
 # Example of manual execution
--- airflow/pipe_naf_reader_dag.py
+++ airflow/pipe_naf_reader_dag.py
@@ -7,6 +7,7 @@ from airflow.contrib.hooks.gcs_hook import GoogleCloudStorageHook
 from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.sensors.base_sensor_operator import BaseSensorOperator
 
+from airflow_ext.gfw import config as config_tools
 from airflow_ext.gfw.models import DagFactory
 
 
@@ -59,69 +60,52 @@ def source_exists_sensor(bucket, prefix, dag):
 
 class NAFReaderDagFactory(DagFactory):
 
-    def __init__(self, pipeline=PIPELINE, **kwargs):
+    def __init__(self, country, pipeline=PIPELINE, **kwargs):
         super(NAFReaderDagFactory, self).__init__(pipeline=pipeline, **kwargs)
-
-    def source_date(self):
-        if self.schedule_interval == '@daily':
-            return '{{ ds }}','{{ ds_nodash }}'
-        else:
-            raise ValueError('Unsupported schedule interval {}'.format(self.schedule_interval))
+        self.country = country
 
     def build(self, dag_id):
+        if self.schedule_interval != '@daily':
+            raise ValueError('Unsupported schedule interval {}'.format(self.schedule_interval))
+
         config = self.config
-        source_date = self.source_date()
-        config['date'] = source_date[0]
-        config['date_nodash'] = source_date[1]
-        print('config = %s' % (config))
-        country_configurations = json.loads(config['configurations'])
+        name = self.config['name']
+        dag_id='{}.{}'.format(dag_id, name)
 
+        with DAG(dag_id, schedule_interval=self.schedule_interval, default_args=self.default_args) as dag:
 
-        naf_reader_image = "{docker_image}".format(**config)
-        print('naf_reader_image = %s' % naf_reader_image)
+            # Checks that the gcs folder exists and contains file for the current {ds}
+            # Question? must be inside the k8s op
+            source_bucket = re.search('(?<=gs://)[^/]*', self.country['gcs_source']).group(0)
+            naf_gcs_path = re.search('(?<=gs://)[^/]*/(.*)', self.country['gcs_source']).group(1)
+            naf_gcs_path = '{}/{ds}'.format(naf_gcs_path, **config)
+            source_exists = source_exists_sensor(
+                bucket=source_bucket,
+                prefix=naf_gcs_path,
+                dag=dag
+            )
 
-        dags=[]
-        for country_config in country_configurations:
-            dag_id='{}.{}'.format(dag_id)
-            naf_reader_args = ['{name}',
-                               '{gcs_source}',
-                               '{gcs_csv_output}',
-                               '{bq_output}',
-                               '{ds}']
-            for arg in range(len(naf_reader_args)):
-                naf_reader_args[arg]=naf_reader_args[arg].format(**config)
-            naf_reader_args.insert(0, 'naf_reader_daily')
-            print('naf_reader_args = %s' % naf_reader_args)
-            naf_reader_args.insert(0, "./scripts/run.sh")
+            naf_reader = KubernetesPodOperator(
+                namespace = os.getenv('K8_NAMESPACE'),
+                image = '{docker_image}'.format(**config),
+                cmds = './scripts/run.sh '
+                    'naf_reader_daily '
+                    '{name} '
+                    '{gcs_source} '
+                    '{gcs_csv_output} '
+                    '{bq_output} '
+                    '{ds}'.format(**config),
+                name = 'naf_reader.{}'.format(name),
+                task_id = "naf-reader-task",
+                get_logs = True,
+                in_cluster = True if os.getenv('KUBERNETES_SERVICE_HOST') else False,
+                dag = dag
+            )
 
-            with DAG(dag_id, schedule_interval=self.schedule_interval, default_args=self.default_args) as dag:
+            dag >> naf_reader >> source_exists
 
-                # Checks that the gcs folder exists and contains file for the current {ds}
-                # Question? must be inside the k8s op
-                source_bucket = re.search('(?<=gs://)[^/]*', country_config['gcs_source']).group(0)
-                naf_gcs_path = re.search('(?<=gs://)[^/]*/(.*)', country_config['gcs_source']).group(1)
-                naf_gcs_path = '{}/{}'.format(naf_gcs_path, config['ds'])
-                source_exists = source_exists_sensor(
-                    bucket=source_bucket,
-                    prefix=naf_gcs_path,
-                    dag=dag
-                )
+            return dag
 
-                naf_reader = KubernetesPodOperator(
-                    namespace=os.getenv('K8_NAMESPACE'),
-                    image=naf_reader_image,
-                    cmds=naf_reader_args,
-                    name='naf_reader.{}'.format(country_config['name']),
-                    task_id="naf-reader-task",
-                    get_logs=True,
-                    in_cluster=True if os.getenv('KUBERNETES_SERVICE_HOST') else False,
-                    dag=dag
-                )
-
-                dag >> naf_reader >> source_exists
-
-                dags.append(dag)
-
-        return dags
-
-naf_reader_daily_dag = NAFReaderDagFactory().build(dag_id='{}_daily'.format(PIPELINE))
+country_configurations = json.loads(config_tools.load_config(pipeline)['configurations'])
+for country_config in country_configurations:
+    naf_reader_daily_dag = NAFReaderDagFactory(country_config).build(dag_id='{}_daily'.format(PIPELINE))
